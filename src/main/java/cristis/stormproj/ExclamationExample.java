9247b4d59be996fa6428d17979a86f99db6c9aae
Cristian Schuszter <cschuszter@metabiota.com>
2017-01-15 10:43:15 +0200

    Add project and exclamation example

---
+++ src/main/java/cristis/stormproj/ExclamationExample.java
@@ -0,0 +1,67 @@
+package cristis.stormproj;
+
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.generated.AlreadyAliveException;
+import org.apache.storm.generated.AuthorizationException;
+import org.apache.storm.generated.InvalidTopologyException;
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.testing.TestWordSpout;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
+import org.apache.storm.utils.Utils;
+
+import java.util.Map;
+
+/**
+ * Created by darkg on 12-Jan-17.
+ */
+
+
+public class ExclamationExample {
+
+    public static class ExclamationBolt extends BaseRichBolt {
+        OutputCollector collector;
+
+        @Override
+        public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
+            this.collector = collector;
+        }
+
+        @Override
+        public void execute(Tuple input) {
+            collector.emit(new Values(input.getString(0) + "!!"));
+            collector.ack(input);
+        }
+
+        @Override
+        public void declareOutputFields(OutputFieldsDeclarer declarer) {
+            declarer.declare(new Fields("word"));
+        }
+
+    }
+
+    public static void main(String[] args) throws InvalidTopologyException, AuthorizationException, AlreadyAliveException {
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout("words", new TestWordSpout(), 10);
+        builder.setBolt("exclaim1", new ExclamationBolt(), 3)
+                .shuffleGrouping("words");
+        builder.setBolt("exclaim2", new ExclamationBolt(), 2)
+                .shuffleGrouping("exclaim1");
+
+        Config conf = new Config();
+        conf.setDebug(true);
+        conf.setNumWorkers(2);
+
+        LocalCluster cluster = new LocalCluster();
+        cluster.submitTopology("test", conf, builder.createTopology());
+        Utils.sleep(10000);
+        cluster.killTopology("test");
+        cluster.shutdown();
+    }
+}
